8296f3711f7441a757a5474a9d4edc0853013ba1
rebel216 <neerajbhardwajpkt@gmail.com>
2022-05-11 22:17:08 +0530

    Add chages in CSS as requested in PR

--- Js/index.js
+++ Js/index.js
@@ -120,25 +120,19 @@ const dataCards = [
 ];
 const WorkDetailsMobile = (index) => `
   <article class="card-work-detail">
-          <button type="button">
-            <span  class="btn-close-detail">
-              <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L7 5.58579L12.2929 0.292893C12.6834 -0.0976311 13.3166 -0.0976311 13.7071 0.292893C14.0976 0.683417 14.0976 1.31658 13.7071 1.70711L8.41421 7L13.7071 12.2929C14.0976 12.6834 14.0976 13.3166 13.7071 13.7071C13.3166 14.0976 12.6834 14.0976 12.2929 13.7071L7 8.41421L1.70711 13.7071C1.31658 14.0976 0.683417 14.0976 0.292893 13.7071C-0.0976311 13.3166 -0.0976311 12.6834 0.292893 12.2929L5.58579 7L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z"
-                  fill="#67798E"></path>
-              </svg>
-            </span>
+          <div>
+          <button type="button" class="btn-close-detail">
+            <img src="./asset/Icon-Cancel.svg" alt="cancel icon">
           </button>
+          </div>
           <div>
           <div class="card-image-details">
               <img src=${dataCards[index].url} alt="cat cute">
           </div>
           </div>
-          <div class="card-title-mobile">
-          <h2>
+          <h2 class="card-title-mobile">
               ${dataCards[index].title}
           </h2>
-          </div>
             <div class="card-links-mobile ">
                   <a href=${dataCards[index].linkVersion} class="btn-card-details-mobile">
                   See Live
@@ -149,7 +143,7 @@ const WorkDetailsMobile = (index) => `
                   <img src="./asset/github_white.svg" alt="cat cute">
                   </a>
           </div>
-          <div class="worktag-popup">
+          <div>
           <ul class="work-tag-popup">
               <li class="mobile-tag">${dataCards[index].tecnologies.tech1}</li>
               <li class="mobile-tag">${dataCards[index].tecnologies.tech2}</li>
