961c2272cf7fcda0417a4441645e7a926ba369f6
rebel216 <neerajbhardwajpkt@gmail.com>
2022-05-12 03:30:26 +0530

    Added validation for mobile form

--- Js/index.js
+++ Js/index.js
@@ -5,19 +5,18 @@ const PortfolioClicked = document.querySelector(".link-portfolio");
 const AboutClicked = document.querySelector(".link-about");
 const ContactClicked = document.querySelector(".link-contact");
 
-var page = document.getElementsByTagName('body')[0];
-
+var page = document.getElementsByTagName("body")[0];
 
 function displaymobilemenu() {
   //alert("changed");
   mobileMenu.classList.replace("mobile-menu-hide", "mobile-menu-show");
-  page.classList.add('noscroll');
+  page.classList.add("noscroll");
 }
 
 function hidemobilemenu() {
   //alert("changed");
   mobileMenu.classList.replace("mobile-menu-show", "mobile-menu-hide");
-  page.classList.remove('noscroll');
+  page.classList.remove("noscroll");
 }
 
 btnMobileMenu.addEventListener("click", displaymobilemenu);
@@ -26,19 +25,25 @@ PortfolioClicked.addEventListener("click", hidemobilemenu);
 AboutClicked.addEventListener("click", hidemobilemenu);
 ContactClicked.addEventListener("click", hidemobilemenu);
 
+/* FORM VALIDATIONS */
 
-  /* FORM VALIDATIONS */
+const submit = document.getElementById("submit-form");
+const submit1 = document.getElementById("submit-form1");
+const emailError = document.getElementById("error");
 
-  const submit = document.getElementById('submit-form');
-  const emailError = document.getElementById('error');
-  
-  submit.addEventListener('click', (event) => {
-    const email = document.getElementById('email-form').value;
-    const emailValidation = /[A-Z]/.test(email);
-  
-    if (emailValidation) {
-      emailError.textContent = 'To submit the form, please enter your email id in lowercase';
-      event.preventDefault();
-    }
-  });
-    
+submit.addEventListener("click", (event) => {
+  const email = document.getElementsById("#email").value;
+  const email1 = document.getElementById("#email-form").value;
+  const emailValidation = /[A-Z]/.test(email);
+  const emailValidation1 = /[A-Z]/.test(email1);
+
+  if (emailValidation) {
+    emailError.textContent =
+      "To submit the form, please enter your email id in lowercase";
+    event.preventDefault();
+  } else if (emailValidation1) {
+    emailError.textContent =
+      "To submit the form, please enter your email id in lowercase";
+    event.preventDefault();
+  }
+});
